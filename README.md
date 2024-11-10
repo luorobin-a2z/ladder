e4c87a09ebe50cd16af2a783918b81c5660ee6be
EC2 Default User <ec2-user@ip-172-31-45-236.ap-southeast-1.compute.internal>
2024-11-10 06:40:45 +0000

    iniital commit

--- README.md
+++ README.md
@@ -1,14 +1,67 @@
-# Welcome to your CDK TypeScript project
+It's an application to deploy instance on AWS provisioning shadowsocks, the ss server uses below obscusion protocols, 
 
-This is a blank project for CDK development with TypeScript.
+- v2ray (port `80`)
+- xray (port `443`)
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Prerequisites
+- An AWS account
+- Nodejs LTS installed, such as 14.x
+- Configure [aws credential][cli-configure-files] for your account. For new AWS user, you can install [awscli][awscli] firstly, then run `aws configure` to set up it. NOTE: make sure granting the credential with adminstrator privillege.
+- A [public hosted zone in Route53][create-public-hosted-zone] for your domain
+- Install dependencies of app  
+```
+yarn install --check-files --frozen-lockfile
+npx projen
+```
+- [Bootstrap AWS CDK environment][cdk-bootstrap](only run once for first time deployment in account and region combination)
+```bash
+npx cdk bootstrap -c hostedZoneName=<r53 hosted zone domain> -c dnsName=<subdomain>
+```
 
-## Useful commands
+## Deploy
+```bash
+npx cdk deploy -c servicePass=<the password> -c hostedZoneName=<r53 hosted zone domain> -c dnsName=<subdomain>
+```
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `npx cdk deploy`  deploy this stack to your default AWS account/region
-* `npx cdk diff`    compare deployed stack with current state
-* `npx cdk synth`   emits the synthesized CloudFormation template
+## Client example
+You can use [Clash][clash] as client to connect the server. The example configuration snippet of Clash looks like below,
+
+```yml
+proxies:
+  # shadowsocks
+  # 支持加密方式：
+  #   aes-128-gcm aes-192-gcm aes-256-gcm
+  #   aes-128-cfb aes-192-cfb aes-256-cfb
+  #   aes-128-ctr aes-192-ctr aes-256-ctr
+  #   rc4-md5 chacha20 chacha20-ietf xchacha20
+  #   chacha20-ietf-poly1305 xchacha20-ietf-poly1305
+
+  - name: "v2ray"
+    type: ss
+    server: <hostname of your server>
+    port: 80
+    cipher: chacha20-ietf-poly1305
+    password: <password of server>
+    plugin: v2ray-plugin
+    plugin-opts:
+      mode: websocket # 暂时不支持 QUIC 协议
+      tls: true # wss
+      skip-cert-verify: true
+      host: <hostname of your server>
+      path: "/"
+```
+
+## Cost
+- default EC2 instance size is `t2.micro`, which is qualified for [AWS Free Tier][free-tier] in first 12 months of new account
+- data transfer out to internet from EC2, [100 GB free per month][dto-free-tier]
+- $0.5 per month for [one Route53 public hosted zone][route53-hosted-zone-price]
+
+[cli-configure-files]: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+[aws-configure]: https://awscli.amazonaws.com/v2/documentation/api/latest/reference/configure/index.html
+[awscli]: https://aws.amazon.com/cli/
+[create-public-hosted-zone]: https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/CreatingHostedZone.html
+[cdk-bootstrap]: https://docs.aws.amazon.com/cdk/latest/guide/bootstrapping.html
+[clash]: https://github.com/Dreamacro/clash
+[free-tier]: https://aws.amazon.com/free/?all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*all&all-free-tier.q=ec2&all-free-tier.q_operator=AND
+[dto-free-tier]: https://aws.amazon.com/blogs/aws/aws-free-tier-data-transfer-expansion-100-gb-from-regions-and-1-tb-from-amazon-cloudfront-per-month/
+[route53-hosted-zone-price]: https://aws.amazon.com/route53/pricing/
--- lib/ladder-stack.ts
+++ lib/ladder-stack.ts
@@ -1,16 +1,159 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as assets from 'aws-cdk-lib/aws-s3-assets';
+import * as path from 'path';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as autoscaling from 'aws-cdk-lib/aws-autoscaling';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+
+
+
 
 export class LadderStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, 'Vpc', {
+      isDefault: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LadderQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const hostedZoneName = this.node.tryGetContext('hostedZoneName');
+    const dnsName = this.node.tryGetContext('dnsName');
+
+    if (!hostedZoneName || !dnsName) {
+      throw new Error('hostedZoneName and dnsName must be provided');
+    }
+
+    const hostedZone = route53.HostedZone.fromLookup(this, 'R53HostedZone', {
+      domainName: hostedZoneName,
+      privateZone: false,
+    });
+    
+    const password = this.node.tryGetContext('password');
+    if (!password) {
+      throw new Error('password must be provided');
+    }
+    const asset = new assets.Asset(this, 'LadderAsset', {
+      path: path.join(__dirname, "docker-compose.yml"),
+    });
+
+    const params: ec2.S3DownloadOptions = {
+      bucket: asset.bucket,
+      bucketKey: asset.s3ObjectKey,
+    };
+    const s3Path = `s3://${params.bucket}/${params.bucketKey}`;
+    const localPath = ( params.localFile && params.localFile.length !== 0 ) ? params.localFile : `/tmp/${ params.bucketKey }`;
+
+    const userData = ec2.UserData.forLinux();
+    userData.addCommands(
+      'set -euxo pipefail',
+      `mkdir -p $(dirname '${localPath}')`,
+      `aws s3 cp '${s3Path}' '${localPath}' --region ${cdk.Aws.REGION}`,
+      'cat > r53-records.json << EOF',
+      '{ "Changes": [ {',
+      '  "Action": "UPSERT",',
+      '  "ResourceRecordSet": {',
+      `  "Name": "${dnsName}",`,
+      '  "Type": "CNAME",',
+      '  "TTL": 300,',
+      '  "ResourceRecords": [ {',
+      '  "Value": "`curl http://169.254.169.254/latest/meta-data/public-hostname`" }',
+      '  ] } ',
+      '} ] }',
+      'EOF',
+      `aws route53 change-resource-record-sets --hosted-zone-id ${hostedZone.hostedZoneId} --change-batch file://r53-records.json`,
+      'yum -y update',
+      'modprobe tcp_bbr && modprobe sch_fq && sysctl -w net.ipv4.tcp_congestion_control=bbr',
+      'amazon-linux-extras install -y docker',
+      'systemctl enable docker',
+      'systemctl start docker',
+      'curl -L "https://github.com/docker/compose/releases/download/v2.24.7/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose',
+      'chmod +x /usr/local/bin/docker-compose',
+      'ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose',
+      'cat > .env << EOF',
+      `PASSWORD=${password}`,
+      `HOST=${dnsName}`,
+      'EOF',
+      'docker network create traefik',
+      `docker-compose --env-file ./.env -f ${localPath} up -d`,
+    );
+
+    const securityGroup = new ec2.SecurityGroup(this, 'LadderSecurityGroup', {
+      vpc,
+      description: 'Security group for Ladder instances',
+      allowAllOutbound: true,
+    });
+
+    // Add inbound rules
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(80),
+      'Allow HTTP'
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(443),
+      'Allow HTTPS'
+    );
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH'
+    );
+
+    const launchTemplate = new ec2.LaunchTemplate(this, 'LadderLaunchTemplate', {
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE3, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      userData,
+      securityGroup,
+      role: new iam.Role(this, 'LadderLaunchTemplateRole', {
+        assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+        inlinePolicies: {
+          'route53': new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ['route53:ChangeResourceRecordSets'],
+                resources: ['hostedZone.hostedZoneArn'],
+              }),
+            ],
+          }),
+          's3': new iam.PolicyDocument({
+            statements: [
+              new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: ['s3:GetObject'],
+                resources: [asset.bucket.arnForObjects(asset.s3ObjectKey)],
+              }),
+            ],
+          }),
+        },
+      }),
+    });
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'LadderAsg', {
+      vpc,
+      launchTemplate,
+      minCapacity: 1,
+      maxCapacity: 1,
+      updatePolicy: autoscaling.UpdatePolicy.rollingUpdate({
+        minInstancesInService: 1,
+        maxBatchSize: 1,
+        pauseTime: cdk.Duration.minutes(3),
+      }),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      instanceMonitoring: autoscaling.Monitoring.BASIC,
+    });
+
+    asg.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+    asg.connections.allowFromAnyIpv4(ec2.Port.tcp(22), 'SSH access');
+    asg.connections.allowFromAnyIpv4(ec2.Port.tcp(80), 'HTTP access');
+    asg.connections.allowFromAnyIpv4(ec2.Port.tcp(443), 'HTTPS access');
+
+    asset.grantRead(asg.role);
   }
 }
